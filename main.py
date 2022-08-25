ab293e948b6ab2b65b48389a7245e82b83384caf
LandedWriter <74064943+LandedWriter0103@users.noreply.github.com>
2022-08-25 19:14:41 +0800

    Add some json file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import json
 import asyncio
 import inspect
 import traceback
@@ -32,6 +33,14 @@ class TestBot(discord.Client):
         ]
         self.nchannel = 1010557329579724864
         self.vcategory = 1003922992805449779
+
+        self.admin_commands = [
+            'send', 'clear' 
+        ]
+        self.vchannel_commands = [
+            'name', 'whitelist', 'blacklist', 'mute', 'unmute', 'deaf', 'undeaf', 'kick', 'coop'
+        ]
+        self.vchannel_detail = 
 
         super().__init__(intents=intents)
 
@@ -93,6 +102,71 @@ class TestBot(discord.Client):
 
 ##############################################################
 
+    """admin only"""
+    async def cmd_send(self, channel, other):
+        """
+        Usage:
+            {command_prefix}send <message_content> [channel_id]
+        Example:
+            Send Hello in current channel | send Hello
+            Send Hello World in Lobby with channel id | send Hello World 1003922992805449780 
+        Change:
+            None
+        Name:
+            send
+        Return:
+            Message Content
+        """
+        content = None
+        
+        # other = [message, channel_id[-1]]
+        try:
+            exp_channel = self.get_channel(int(other[-1]))
+        except:
+            exp_channel = None
+
+        if not exp_channel:
+            exp_channel = channel
+        else:
+            other.pop(-1)
+        
+        content = ' '.join(other)
+
+        await self.channel_send(
+            response=content,
+            type='text',
+            channel=exp_channel
+        )
+
+    async def cmd_clear(self, guild, other):
+        """
+        Usage:
+            {command_prefix}clear <object type>
+        Example:
+            Clear all vchannel which are not in blacklist | clear vchannel
+        Change:
+            None
+        Name:
+            clear
+        Return:
+            None
+        """
+        if not other:
+            raise JustWrong("missing arguments")
+        
+        type = other[0]
+
+        if type == 'vchannel':
+            for channel in guild.voice_channels:
+                await self.del_vchannel(channel)
+            return
+        
+        if type == 'message':
+            return
+
+        print("Unknown Type\n")
+
+    """global"""
     async def cmd_help(self):
         pass
 
@@ -177,68 +251,7 @@ class TestBot(discord.Client):
         embed.title = f'{emoji_prefix} {num}'
         return embed
 
-    async def cmd_send(self, channel, other):
-        """
-        Usage:
-            {command_prefix}send <message_content> [channel_id]
-        Example:
-            Send Hello in current channel | send Hello
-            Send Hello World in Lobby with channel id | send Hello World 1003922992805449780 
-        Change:
-            None
-        Name:
-            send
-        Return:
-            Message Content
-        """
-        content = None
-        
-        # other = [message, channel_id[-1]]
-        try:
-            exp_channel = self.get_channel(int(other[-1]))
-        except:
-            exp_channel = None
 
-        if not exp_channel:
-            exp_channel = channel
-        else:
-            other.pop(-1)
-        
-        content = ' '.join(other)
-
-        await self.channel_send(
-            response=content,
-            type='text',
-            channel=exp_channel
-        )
-
-    async def cmd_clear(self, guild, other):
-        """
-        Usage:
-            {command_prefix}clear <object type>
-        Example:
-            Clear all vchannel which are not in blacklist | clear vchannel
-        Change:
-            None
-        Name:
-            clear
-        Return:
-            None
-        """
-        if not other:
-            raise JustWrong("missing arguments")
-        
-        type = other[0]
-
-        if type == 'vchannel':
-            for channel in guild.voice_channels:
-                await self.del_vchannel(channel)
-            return
-        
-        if type == 'message':
-            return
-
-        print("Unknown Type\n")
 
 ##############################################################
 
@@ -257,7 +270,7 @@ class TestBot(discord.Client):
             content, color = f'{member.display_name} | 加入 {str(after.channel)[len(self.vprefix)-1:]}', (150, 194, 208)
             
         elif not join and leave: # change
-            content, color = f'{member.display_name} | 離開 | {str(before.channel)[len(self.vprefix)-1:]}', None
+            content, color = f'{member.display_name} | 離開 {str(before.channel)[len(self.vprefix)-1:]}', None
 
         else: # leave only
             content, color = f'{member.display_name} | {str(before.channel)[len(self.vprefix)-1:]} 移到 {str(after.channel)[len(self.vprefix)-1:]}', (216, 176, 107)
@@ -347,9 +360,31 @@ class TestBot(discord.Client):
             traceback.print_exc()
             print("\nRaised unknown error on getting command.\n")
 
+        is_admin = message.author.guild_permissions.administrator
+
         argspec = inspect.signature(target)
         params = argspec.parameters.copy()
-        
+
+        if (
+            command in self.admin_commands
+            and not is_admin
+        ):
+            await self.channel_send(
+                "This is an admin only command.",
+                type='text', channel=message.channel
+            )
+            return
+
+        if (
+            command in self.vchannel_commands
+            and message.channel.type != 'voice'
+        ):
+            await self.channel_send(
+                "This is a vchannel command.",
+                type='text', channel=message.channel
+            )
+            return
+
         target_kwargs = {}
         if params.pop('message', None):
             target_kwargs['message'] = message
@@ -455,9 +490,8 @@ class TestBot(discord.Client):
         # cannot use if limit: because 0 also equals to false
         if limit == '':
             await self.channel_send(
-                response='再多一個 | 我就幫你折蓮花',
-                type='text',
-                channel=channel
+                "再多一個 | 我就幫你折蓮花",
+                type='text', channel=channel
             )
             return
 
@@ -611,4 +645,3 @@ class TestBot(discord.Client):
             return 256000
         if tier == 3:
             return 384000
-             
